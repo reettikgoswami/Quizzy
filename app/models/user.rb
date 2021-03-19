94a114c8ccfcd9aaf32453911980ad0bd95b7592
reettik97 <reettikgoswami1997@gmail.com>
2021-03-19 17:05:28 +0530

    Reorder of items on Active Record Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,20 @@
 
 class User < ApplicationRecord
   has_many :quizzes, dependent: :destroy
-  before_save :downcase_email
 
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  
   enum role: %i(standard administrator)
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  uniqueness: { case_sensitive: false }
   validates :first_name, presence: true, length: { maximum: 50 }                  
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, on: :create
+  
+  before_save :downcase_email
+  
   has_secure_password
 
   private
--- db/migrate/20210315155138_create_users.rb
+++ db/migrate/20210315155138_create_users.rb
@@ -1,12 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email, null: false, unique: true
+      t.string :email, null: false, index: { unique: true }
       t.string :first_name, null: false
       t.string :last_name, null: false
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
   end
 end
