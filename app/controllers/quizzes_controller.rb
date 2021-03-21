82af8fa380769a130e0ebb9409dabe3554b7b564
reettik97 <reettikgoswami1997@gmail.com>
2021-03-21 23:05:30 +0530

    Added route for edit delete and show quiz

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,11 @@
 class QuizzesController < ApplicationController
   
-  before_action :logged_in_user, only: [:create]
+  before_action :logged_in_user, only: [:create, :index, :show, :update]
+
+  def index 
+    @quizzes = Quiz.order('created_at DESC')
+    render status: :ok, json: { quizzes: @quizzes }
+  end
 
   def create
     @quiz = current_user.quizzes.build(quiz_params)
@@ -8,7 +13,35 @@ class QuizzesController < ApplicationController
     if @quiz.save 
       render status: :ok, json: { success: "Quiz successfully created!" }
     else
-      render status: :unprocessable_entity, json: { errors: @quiz.errro.full_messages }
+      render status: :unprocessable_entity, json: { errors: @quiz.errors.full_messages }
+    end
+  end
+
+  def show 
+    @quiz = Quiz.find(params[:id]) 
+    
+    if @quiz.present?
+      render status: :ok, json: { quiz: @quiz }
+    else
+      render :json => {:error => "not-found"}.to_json, :status => 404
+    end
+  end
+
+  def update
+    @quiz = Quiz.find(params[:id])
+    if @quiz.update(quiz_params)
+      render status: :ok, json: { success: "Quiz updated successfully." }
+    else
+      render status: :unprocessable_entity, json: { errors: @quiz.errors.full_messages }
+    end
+  end
+
+  def destroy
+    @quiz = Quiz.find(params[:id])
+    if @quiz.destroy
+      render status: :ok, json: { success: "Quiz deleted successfully" }
+    else
+      render status: :unprocessable_entity, json: { errors: @quiz.errors.full_messages }
     end
   end
 
--- app/javascript/src/apis/quiz.js
+++ app/javascript/src/apis/quiz.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 
+const list = () => axios.get("/quizzes");
+
+const show = id => axios.get(`/quizzes/${id}`);
+
 const create = payload => axios.post("/quizzes", payload);
 
+const update = (id, payload) => axios.put(`/quizzes/${id}`, payload);
+
+const destroy = id => axios.delete(`/quizzes/${id}`);
+
 const quizApi = {
+  list,
   create,
+  show,
+  destroy,
+  update,
 };
 
 export default quizApi;
