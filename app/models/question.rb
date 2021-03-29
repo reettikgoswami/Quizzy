1ee8d02417c311dfeadf76ada92b39a673a332ff
reettik97 <reettikgoswami1997@gmail.com>
2021-03-29 12:02:39 +0530

    Add custom validation in question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,38 @@ class Question < ApplicationRecord
 
   validates :description, presence: true
   validates :quiz_id, presence: true
-  accepts_nested_attributes_for :options, allow_destroy: true
+
+  accepts_nested_attributes_for :options,
+                                  reject_if: proc { |attributes| attributes["value"].blank?}, allow_destroy: true
+
+  validate :question_must_have_a_correct_answer, :options_limit
+
+
+  def options_limit
+    if options.length < 2
+      errors.add(:question, "must_have at least 2 options.")
+    end
+    if options.length > 4
+      errors.add(:question, "must_have at most 4 options.")
+    end
+  end
+
+  def question_must_have_a_correct_answer
+    correct_answer_count = 0
+
+    options.each do |option|
+    correct_answer_count += 1 if option.is_correct
+    end
+
+    if correct_answer_count == 0
+      errors.add(:question, "must_have_a_correct_answer.")
+      
+    end
+    if correct_answer_count > 1 
+        errors.add(:question,  "must have only one correct answer.")
+    end
+  end
+  
+
+
 end
